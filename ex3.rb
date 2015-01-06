17d69e508f4db886f4cfe38edc74ac6f7205de18
MitsunChieh <Mitsun.Chieh@gmail.com>
2015-01-06 15:09:51 +0800

    finish ex3.

---
+++ ex3.rb
@@ -0,0 +1,24 @@
+puts "I will now count my chickens." # print out string on display
+
+puts "Hens #{ 25 + 30 / 6 }" # 25 + 5 = 30
+puts "Roosters #{ 100 - 25 * 3 % 4 }" # 100 - 75 % 4 = 100 - 3 = 97
+
+puts "Now I will count the eggs." # print out string on display
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6 # 6 - 5 + 0 - 0(0.25) + 6 = 7(6.75)
+
+puts "Is it true that 3 + 2 < 5 - 7?" # print out string on display
+
+puts 3 + 2 < 5 - 7 # If 5 is less than -2, return true
+
+puts "What is 3 + 2? #{ 3 + 2 }" # 3 + 2 = 5
+puts "What is 5 - 7? #{ 5 - 7 }" # 5 - 7 = -2
+
+puts "Oh, that's why it's false." # 5 is more than -2, not less than. So return false
+
+puts "How about some more." # print out string on display
+
+puts "Is it greater? #{  5 > -2 }"
+puts "Is it great or equal? #{ 5 >= -2 }"
+puts "Is it less or equal? #{ 5 <= -2 }"
+
---
+++ ex3_2.rb
@@ -0,0 +1,2 @@
+puts 4 - 4 > -4 + 4
+puts "aaa" > "aa"
